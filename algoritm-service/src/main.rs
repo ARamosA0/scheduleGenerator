37668ec06d5db63b8721e89e5241883c0f65cb1d
ARamosA0 <aldoramosaro@gmail.com>
2025-06-12 20:37:36 -0500

    Demo para el algoritmo

--- algoritm-service/src/main.rs
+++ algoritm-service/src/main.rs
@@ -1,17 +1,35 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use rocket::serde::json::Json;
+
+mod algoritm;
+
+use algoritm::demo::{run_genetic_algorithm, BestSolutionResult};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ResultData {
+    x: u32,
+    fx: i32,
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "¡Hola, mundo desde Rocket! watch"
+    "¡Hola, mundo desde Rocket!"
+}
+
+#[get("/run-algorithm")]
+fn run_algorithm() -> Json<BestSolutionResult> {
+    Json(run_genetic_algorithm())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .configure(rocket::Config {
-            address: std::net::Ipv4Addr::new(0, 0, 0, 0).into(),  // Escucha en 0.0.0.0
-            port: 8088,  // Asegúrate de que coincida con el puerto expuesto
-            ..rocket::Config::default()
-        })
+    let config = rocket::Config {
+        address: "0.0.0.0".parse().unwrap(),
+        port: 8088,
+        ..Default::default()
+    };
+
+    rocket::custom(config).mount("/", routes![index, run_algorithm])
 }
